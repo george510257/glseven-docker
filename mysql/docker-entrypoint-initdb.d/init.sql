3857a8138a2bf907c1474b14a66c9d38aa9f45d2
李智勇 <george510257@gmail.com>
2024-05-05 13:02:34 +0800

    修改keycloak

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -1,9 +1,3 @@
--- 创建数据库 keycloak
-CREATE DATABASE `keycloak` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
--- 创建普通用户 keycloak
-CREATE USER `keycloak`@`%` IDENTIFIED WITH mysql_native_password BY 'keycloak';
-GRANT ALL PRIVILEGES ON `keycloak`.* TO `keycloak`@`%`;
-
 -- 创建数据库 xxl_job
 CREATE DATABASE `xxl_job` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 -- 创建普通用户 xxl_job
