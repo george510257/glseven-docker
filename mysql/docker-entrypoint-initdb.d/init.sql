45e98a9015e52653a5c3cbf39863c3c5248cfcff
george <george510257@gmail.com>
2025-10-25 12:21:39 +0800

    feat(mysql): 更新用户认证插件为 caching_sha2_password- 将 xxl_job 用户的认证插件从 mysql_native_password 更改为 caching_sha2_password- 将 nacos 用户的认证插件从 mysql_native_password 更改为 caching_sha2_password- 保持原有权限配置不变- 添加 flush privileges 确保权限生效

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -1,13 +1,13 @@
 -- 创建数据库 xxl_job
 create database `xxl_job` character set 'utf8mb4' collate 'utf8mb4_general_ci';
 -- 创建普通用户 xxl_job
-create user `xxl_job`@`%` identified with mysql_native_password by 'xxl_job';
+create user `xxl_job`@`%` identified with caching_sha2_password by 'xxl_job';
 grant all privileges on `xxl_job`.* to `xxl_job`@`%`;
 
 -- 创建数据库 nacos_devtest
 create database `nacos_devtest` character set 'utf8mb4' collate 'utf8mb4_general_ci';
 -- 创建普通用户 nacos
-create user `nacos`@`%` identified with mysql_native_password by 'nacos';
+create user `nacos`@`%` identified with caching_sha2_password by 'nacos';
 grant all privileges on `nacos_devtest`.* to `nacos`@`%`;
 
 flush privileges;
